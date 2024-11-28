8f5b1f0280d6283b3fec7a13d7fb20f29da5bcd8
6704062611298 <s6704062611298@email.kmutnb.ac.th>
2024-11-28 14:26:08 +0700

    test

---
+++ lab1minx.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+int main(){
+	int n1=5;
+	int n2=10;
+	if(n1>n2){
+		printf("num1 greater than num2");
+	}
+	else {
+		printf("num2 greater than num1");
+	}
+	return 0;
+}
